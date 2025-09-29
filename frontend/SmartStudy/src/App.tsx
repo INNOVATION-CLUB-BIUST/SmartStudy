d2a27458754b7bea9d3b8bf4555561ce86e71e17
WyvernPirate <moloieric80@gmail.com>
2025-09-29 18:12:35 +0200

    test 2

--- frontend/SmartStudy/src/App.tsx
+++ frontend/SmartStudy/src/App.tsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save
+          Edit <code>src/App.tsx</code> and save it so see changes
         </p>
       </div>
       <p className="read-the-docs">
