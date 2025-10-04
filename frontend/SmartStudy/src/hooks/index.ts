559cc1e382ff0b9033e108ff11b3426b331888ad
WyvernPirate <moloieric80@gmail.com>
2025-10-04 16:16:02 +0200

    use contexts

---
+++ frontend/SmartStudy/src/hooks/index.ts
@@ -0,0 +1,10 @@
+// Export all custom hooks
+// export { useUser } from './useUser';
+// export { useCourses } from './useCourses';
+//export { useAssignments } from './useAssignments';
+//port { useSessions } from './useSessions';
+//export { useUI, useNotifications } from './useUI';
+//export { useTimer } from './useTimer';
+
+// Re-export the main context hook
+export { default as useAppContext } from '../context/AppContext';
